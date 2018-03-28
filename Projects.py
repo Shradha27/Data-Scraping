8fd5a0c892cebe89eac9a8bd565079a6161125fb
Shradha27 <shradha2704@gmail.com>
2018-03-28 23:48:18 +0530

    Projects list

--- Projects.py
+++ Projects.py
@@ -5,23 +5,29 @@ from bs4 import NavigableString
 
 
 def crawler():
-        url = 'https://innovate.mygov.in/sih2018-search'
-        source_code = requests.get(url)
-        text = source_code.text
-        soup = BeautifulSoup(text, "html.parser")
-        nos = []
-        for div in soup.find_all(string=re.compile("Total Submissions")):
-                txt = str(div)
-                no = ""
-                for i, c in enumerate(txt):
-                        if i == 41 or i == 42 or i == 43:
-                                no = no + c
-                nos.append(int(no))
-        for i in soup.findAll('div', {'class': 'head-part'}):
-                if nos[i] < 10:
-                    ts = i.next_sibling.div.get_text()
-                    link = i.div.a.get('href')
-                    title = i.div.a.string
-                    print(title, link)
+    url = 'https://innovate.mygov.in/sih2018-search'
+    source_code = requests.get(url)
+    text = source_code.text
+    soup = BeautifulSoup(text, "html.parser")
+
+    nos = []
+    for div in soup.find_all(string=re.compile("Total Submissions")):
+        txt = str(div)
+        no = ""
+        for i, c in enumerate(txt):
+            if i == 41 or i == 42 or i == 43:
+                no = no + c
+        nos.append(int(no))
+
+    href = []
+    titles = []
+    for div in soup.find_all("div", class_="title"):
+        href.append(div.a.get('href'))
+        titles.append(div.a.get_text())
+
+    for i in range(len(titles)):
+        if nos[i] < 10:
+            print(nos[i], ":" + titles[i] + ":" + href[i])
+
 
 crawler()
