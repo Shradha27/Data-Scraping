6370dc0bf042ab7af8cbb465fcfa693216d24d41
Shradha27 <shradha2704@gmail.com>
2018-03-29 23:47:36 +0530

    Optimized

--- Projects.py
+++ Projects.py
@@ -1,33 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
-import re
-from bs4 import NavigableString
 
 
 def crawler():
-    url = 'https://innovate.mygov.in/sih2018-search'
-    source_code = requests.get(url)
-    text = source_code.text
-    soup = BeautifulSoup(text, "html.parser")
-
-    nos = []
-    for div in soup.find_all(string=re.compile("Total Submissions")):
-        txt = str(div)
-        no = ""
-        for i, c in enumerate(txt):
-            if i == 41 or i == 42 or i == 43:
-                no = no + c
-        nos.append(int(no))
-
-    href = []
-    titles = []
-    for div in soup.find_all("div", class_="title"):
-        href.append(div.a.get('href'))
-        titles.append(div.a.get_text())
-
-    for i in range(len(titles)):
-        if nos[i] < 10:
-            print(nos[i], ":" + titles[i] + ":" + href[i])
-
-
+        url = 'https://innovate.mygov.in/sih2018-search'
+        source_code = requests.get(url)
+        text = source_code.text
+        soup = BeautifulSoup(text, "html.parser")
+        for i in soup.findAll('div', {'class': 'head-part'}):
+                tss=i.find('div', {'class':'desc-info'})
+                h=i.div.string
+                print(h)
+                a=tss.get_text()
+                a=str(a)
+                print(a)
+                b=a[172:175]
+                print(b)
+                b=int(b)
+                print(b)
+                '''  if(b<=100):
+                   link = i.a.get('href')
+                   title = i.a.string
+                   print(title, link) '''
+                
 crawler()
